57d611e73b30f0efb6a8e1b76d8d3e14408adb83
sultanjawer <sultanjawer@gmail.com>
2023-07-24 18:32:50 +0700

    sidebar solutions

--- solutions/sidebar.php
+++ solutions/sidebar.php
@@ -9,7 +9,7 @@
 				<div class="post-item mt-3">
 					<img src="../assets/img/blog/blog-recent-1.jpg" alt="" class="flex-shrink-0">
 					<div>
-						<h4><a href="news-single.php">Nihil blanditiis at in nihil autem</a></h4>
+						<h4><a href="../solutions/solutions-single.php">Nihil blanditiis at in nihil autem</a></h4>
 						<time datetime="2020-01-01">Jan 1, 2020</time>
 					</div>
 				</div><!-- End recent post item-->
@@ -17,7 +17,7 @@
 				<div class="post-item">
 					<img src="../assets/img/blog/blog-recent-2.jpg" alt="" class="flex-shrink-0">
 					<div>
-						<h4><a href="news-single.php">Quidem autem et impedit</a></h4>
+						<h4><a href="../solutions/solutions-single.php">Quidem autem et impedit</a></h4>
 						<time datetime="2020-01-01">Jan 1, 2020</time>
 					</div>
 				</div><!-- End recent post item-->
@@ -25,7 +25,7 @@
 				<div class="post-item">
 					<img src="../assets/img/blog/blog-recent-3.jpg" alt="" class="flex-shrink-0">
 					<div>
-						<h4><a href="news-single.php">Id quia et et ut maxime similique occaecati ut</a></h4>
+						<h4><a href="../solutions/solutions-single.php">Id quia et et ut maxime similique occaecati ut</a></h4>
 						<time datetime="2020-01-01">Jan 1, 2020</time>
 					</div>
 				</div><!-- End recent post item-->
@@ -33,7 +33,7 @@
 				<div class="post-item">
 					<img src="../assets/img/blog/blog-recent-4.jpg" alt="" class="flex-shrink-0">
 					<div>
-						<h4><a href="news-single.php">Laborum corporis quo dara net para</a></h4>
+						<h4><a href="../solutions/solutions-single.php">Laborum corporis quo dara net para</a></h4>
 						<time datetime="2020-01-01">Jan 1, 2020</time>
 					</div>
 				</div><!-- End recent post item-->
@@ -41,7 +41,7 @@
 				<div class="post-item">
 					<img src="../assets/img/blog/blog-recent-5.jpg" alt="" class="flex-shrink-0">
 					<div>
-						<h4><a href="news-single.php">Et dolores corrupti quae illo quod dolor</a></h4>
+						<h4><a href="../solutions/solutions-single.php">Et dolores corrupti quae illo quod dolor</a></h4>
 						<time datetime="2020-01-01">Jan 1, 2020</time>
 					</div>
 				</div><!-- End recent post item-->
