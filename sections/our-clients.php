f38882d73ce612f36d646a32b50e06971c13fa8a
sultanjawer <sultanjawer@gmail.com>
2023-08-05 23:04:24 +0700

    client logo slider now show more icon

--- sections/our-clients.php
+++ sections/our-clients.php
@@ -5,19 +5,30 @@
 			<h2>Our Clients</h2>
 		</div>
 		<?php
-		$folder = 'assets/client/'; // Specify the directory path where your images are located
+		$folder = 'assets/client/';
 		$images = glob($folder . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-		natcasesort($images); // Sort the images array by name
+		natcasesort($images);
+		$imageChunks = array_chunk($images, 6); // Split images into chunks of 6
 		?>
 
 		<div class="clients-slider swiper">
 			<div class="swiper-wrapper align-items-center">
-				<?php foreach ($images as $image) { ?>
+				<?php foreach ($imageChunks[0] as $image) { ?>
 					<div class="swiper-slide">
 						<img class="img-fluid" src="<?php echo $image; ?>" />
 					</div>
 				<?php } ?>
 			</div>
 		</div>
+		<div class="clients-slider swiper">
+			<div class="swiper-wrapper align-items-center">
+				<?php foreach ($imageChunks[1] as $image) { ?>
+					<div class="swiper-slide">
+						<img class="img-fluid" src="<?php echo $image; ?>" />
+					</div>
+				<?php } ?>
+			</div>
+		</div>
+
 	</div>
 </section>
