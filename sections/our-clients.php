d9e4a382e46bc2e1d757244a830a8fe457460dfa
sultanjawer <sultanjawer@gmail.com>
2023-08-06 00:43:44 +0700

    image chunk 2

--- sections/our-clients.php
+++ sections/our-clients.php
@@ -5,28 +5,21 @@
 			<h2>Our Clients</h2>
 		</div>
 		<?php
-		$folder = 'assets/client/';
+		$folder = 'assets/client/'; // Specify the directory path where your images are located
 		$images = glob($folder . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-		natcasesort($images);
-		$imageCount = count($images);
-		$imagesPerRow = ceil($imageCount / 2); // Calculate how many images to show per row
+		natcasesort($images); // Sort the images array by name
+		$imageChunks = array_chunk($images, 2);
 		?>
 
 		<div class="clients-slider swiper">
 			<div class="swiper-wrapper align-items-center">
-				<?php for ($i = 0; $i < $imagesPerRow; $i++) { ?>
+				<?php foreach ($imageChunks as $index) { ?>
 					<div class="swiper-slide">
-						<img class="img-fluid" src="<?php echo $images[$i]; ?>" />
-					</div>
-				<?php } ?>
-			</div>
-		</div>
-
-		<div class="clients-slider swiper">
-			<div class="swiper-wrapper align-items-center">
-				<?php for ($i = $imagesPerRow; $i < $imageCount; $i++) { ?>
-					<div class="swiper-slide">
-						<img class="img-fluid" src="<?php echo $images[$i]; ?>" />
+						<div class="row">
+							<?php foreach ($index as $image) { ?>
+								<img class="img-fluid" src="<?php echo $image; ?>">
+							<?php } ?>
+						</div>
 					</div>
 				<?php } ?>
 			</div>
